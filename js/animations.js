64697a51234488d09019ffcd1f1c8f9ef1d85663
unknown <artem_larin@yoniti.ru>
2023-01-22 19:53:57 +0300

    added other option input

--- js/animations.js
+++ js/animations.js
@@ -17,3 +17,25 @@ function initIncreaseNumberAnimation() {
     increaseNumberAnimationStep(0, element, 5000)
 }
 initIncreaseNumberAnimation();
+
+document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
+    
+    if (event.target.value === "other"){
+        let formContainer = document.createElement('div');
+        formContainer.classList.add('form__group');
+        formContainer.classList.add('form__other-input');
+
+        let input = document.createElement('input')
+        input.placeholder = `Ваш вариант`
+        input.type = 'text'
+
+        formContainer.appendChild(input);
+        document.querySelector('form').insertBefore(formContainer, document.querySelector('.form__submit')); 
+    }
+
+    let otherInput = document.querySelector('.form__other-input')
+    if (event.target.value !== 'other' && Boolean(otherInput)){
+        
+        document.querySelector('form').removeChild(otherInput)
+    }
+  });
