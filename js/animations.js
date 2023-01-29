ed552cdf8e5661c1db0fa24ed8717234e5cf477c
unknown <artem_larin@yoniti.ru>
2023-01-29 23:26:34 +0300

    add scroll animation

--- js/animations.js
+++ js/animations.js
@@ -16,7 +16,8 @@ function initIncreaseNumberAnimation() {
     let element = document.querySelector(".features__clients-count");
     increaseNumberAnimationStep(0, element, 5000)
 }
-initIncreaseNumberAnimation();
+
+
 
 document.querySelector('#budget').addEventListener('change', function handleSelectChange(event) {
     
@@ -39,3 +40,21 @@ document.querySelector('#budget').addEventListener('change', function handleSele
         document.querySelector('form').removeChild(otherInput)
     }
   });
+
+  //AnimationScroll
+  let animationInited = false;
+ window.addEventListener('scroll', updateScroll)
+
+ function updateScroll() {
+    if (window.scrollY > 0){
+        document.querySelector('header').classList.add('header__scrolled')
+    } else {
+        document.querySelector('header').classList.remove('header__scrolled')
+    }
+    let windowBottomPosition = window.scrollY + window.innerHeight;
+    let countElementPosition = document.querySelector('.features__clients-count').offsetTop;
+    if (windowBottomPosition > countElementPosition && !animationInited) {
+        animationInited = true;
+        initIncreaseNumberAnimation()
+    }
+}
